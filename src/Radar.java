bdf740a973ce3a9593d3e4aad8abfb996250123e
Calvin Owens <calvinmowens@gmail.com>
2021-08-31 13:42:19 -0400

    my first Swing GUI

--- src/Radar.java
+++ src/Radar.java
@@ -1,7 +1,28 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Radar {
 
     public static void main(String[] args) {
-        System.out.println("This is the first line of my Radar project.");
+
+        JFrame myFrame = new JFrame();
+
+        JButton start = new JButton("start");
+        start.setBounds(10, 10, 100, 100);
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Start Pressed!!");
+            }
+        });
+
+        myFrame.add(start);
+
+        myFrame.setSize(400, 500);
+        myFrame.setLayout(null);
+        myFrame.setVisible(true);
+
     }
-    
+
 }
